364f66a33629f1bff06f4a49f8e6cd4d67929b7e
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-04 09:28:46 +0200

    Romans are great

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,42 @@
 use std::fmt::{Display, Formatter, Result};
 
-pub struct Roman;
+pub struct Roman {
+    roman: String,
+}
 
 impl Display for Roman {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> Result {
-        unimplemented!("Return a roman-numeral string representation of the Roman object");
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{}", self.roman)
     }
 }
 
 impl From<u32> for Roman {
     fn from(num: u32) -> Self {
-        unimplemented!("Construct a Roman object from the '{num}' number");
+        let roman_map = vec![
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ];
+
+        let mut remaining_num = num;
+        let mut roman_string = String::new();
+        for (value, symbol) in &roman_map {
+            while remaining_num >= *value {
+                roman_string += symbol;
+                remaining_num -= *value;
+            }
+        }
+
+        Roman { roman: roman_string }
     }
 }
